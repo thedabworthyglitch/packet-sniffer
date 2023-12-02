8901ea4d16d554f285d2c0c7c682d034a1153ae6
Utkarsh Parashar <54283883+thedabworthyglitch@users.noreply.github.com>
2023-12-03 03:44:48 +0530

    cleaned up and added dramatic pauses

--- goodsniffer.py
+++ goodsniffer.py
@@ -2,11 +2,15 @@ import socket
 import struct
 import textwrap
 import time
+import os
 
 interfaceip = '192.168.1.8'
 #interfaceip = str(input('Enter the IP to your interface'))
 
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("WELCOME TO THE", end="")
+    time.sleep(1)
     print(logo)
     try:
         conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
@@ -15,7 +19,7 @@ def main():
         conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
         serial_number = 0
-
+        time.sleep(2)
         while True:
             raw_data, addr = conn.recvfrom(65536)
             dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
@@ -80,38 +84,6 @@ def get_protocol(proto):
         0: 'HOPOPT',1: 'ICMP',2:'IGMP',3:'GGP',4:'IPv4',5:'ST',6:'TCP',7:'CBT',8:'EGP',9:'IGP',10:'BBN-RCC-MON',11:'NVP-II',12:'PUP',13:'ARGUS (deprecated)',14:'EMCON',15:'XNET',16:'CHAOS',17:'UDP',18:'MUX',19:'DCN-MEAS',20:'HMP',21:'PRM',22:'XNS-IDP',23:'TRUNK-1',24:'TRUNK-2',25:'LEAF-1',26:'LEAF-2',27:'RDP',28:'IRTP',29:'ISO-TP4',30:'NETBLT',31:'MFE-NSP',32:'MERIT-INP',33:'DCCP',34:'3PC',35:'IDPR',36:'XTP',37:'DDP',38:'IDPR-CMTP',39:'TP++',40:'IL',41:'IPv6',42:'SDRP',43:'IPv6-Route',44:'IPv6-Frag',45:'IDRP',46:'RSVP',47:'GRE',48:'DSR',49:'BNA',50:'ESP',51:'AH',52:'I-NLSP',53:'SWIPE (deprecated)',54:'NARP',55:'Min-IPv4',56:'TLSP',57:'SKIP',58:'IPv6-ICMP',59:'IPv6-NoNxt',60:'IPv6-Opts',61:'any host internal protocol',62:'CFTP',63:'any local network',64:'SAT-EXPAK',65:'KRYPTOLAN',66:'RVD',67:'IPPC',68:'any distributed file system',69:'SAT-MON',70:'VISA',71:'IPCV',72:'CPNX',73:'CPHB',74:'WSN',75:'PVP',76:'BR-SAT-MON',77:'SUN-ND',78:'WB-MON',79:'WB-EXPAK',80:'ISO-IP',81:'VMTP',82:'SECURE-VMTP',83:'VINES',84:'IPTM',85:'NSFNET-IGP',86:'DGP',87:'TCF',88:'EIGRP',89:'OSPFIGP',90:'Sprite-RPC',91:'LARP',92:'MTP',93:'AX.25',94:'IPIP',95:'MICP (deprecated)',96:'SCC-SP',97:'ETHERIP',98:'ENCAP',99:'any private encryption scheme',100:'GMTP',101:'IFMP',102:'PNNI',103:'PIM',104:'ARIS',105:'SCPS',106:'QNX',107:'A/N',108:'IPComp',109:'SNP',110:'Compaq-Peer',111:'IPX-in-IP',112:'VRRP',113:'PGM',114:'any 0-hop protocol',115:'L2TP',116:'DDX',117:'IATP',118:'STP',119:'SRP',120:'UTI',121:'SMP',122:'SM (deprecated)',123:'PTP',124:'ISIS over IPv4',125:'FIRE',126:'CRTP',127:'CRUDP',128:'SSCOPMCE',129:'IPLT',130:'SPS',131:'PIPE',132:'SCTP',133:'FC',134:'RSVP-E2E-IGNORE',135:'Mobility Header',136:'UDPLite',137:'MPLS-in-IP',138:'manet',139:'HIP',140:'Shim6',141:'WESP',142:'ROHC',143:'Ethernet',144:'AGGFRAG',145:'NSH',253:'Used for experimentation and testing',254:'Used for experimentation and testing',255:'Reserved',
     }
     return protocol_map.get(proto, str(proto))
-
-'''
-def get_ports(packet):
-    source_port = packet.sport
-    destination_port = packet.dport
-    return source_port, destination_port
-
-def get_source_port(packet):
-    return get_ports(packet)[0]
-
-def get_destination_port(packet):
-    return get_ports(packet)[1]
-
-'''
-'''def get_source_port(proto, data):
-    if proto == 6:  # TCP
-        return struct.unpack('! H', data[:2])[0]
-    elif proto == 17:  # UDP
-        return struct.unpack('! H', data[:2])[0]
-    else:
-        return None
-
-def get_destination_port(proto, data):
-    if proto == 6:  # TCP
-        return struct.unpack('! H', data[2:4])[0]
-    elif proto == 17:  # UDP
-        return struct.unpack('! H', data[2:4])[0]
-    else:
-        return None
-if __name__ == "__main__":
-    main()
-'''
 
 def get_source_port(data):
     if True:  # TCP
