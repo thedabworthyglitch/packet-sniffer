b61c299f27261c2c105621e9fd259bb6a1ae1488
Utkarsh Parashar <54283883+thedabworthyglitch@users.noreply.github.com>
2023-12-04 00:09:03 +0530

    okay maybe this is it

--- goodsniffer.py
+++ goodsniffer.py
@@ -1,14 +1,25 @@
 import socket
 import struct
 import signal
+import sys
 import time
 import os
 import argparse
 import csv
 
-interfaceip = '192.168.1.8'
-#interfaceip = str(input('Enter the IP to your interface: '))
+#interfaceip = '192.168.1.8'
+interfaceip = str(input('Enter the IP to your interface: '))
+os.system('cls' if os.name == 'nt' else 'clear')
+print("Initializing interface")
+#animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
 
+for i in range(len(animation)):
+    time.sleep(0.2)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+
+print("\n")
 def ethernet_frame(data):
     dest_mac, src_mac, eth_proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(eth_proto), data[14:]
@@ -57,76 +68,65 @@ def write_to_csv(output_file, packet_data):
         writer.writerow(packet_data)
 
 logo = r'''
-                                @@@@@@                                     @@@@@@@@
-                            @@@@@@@@@@@         @                  @     @@@@@@@@@@@@@@
-                       @@@@@@@@@@@@@@@@        @@@                @@@     @@@@@@@@@@@@@@@
-                    @@@@@@@@@@@@@@@@@@       @@@@@@             @@@@@@     @@@@@@   @@@@@@@
-                  @@@@@@@@@@@ @@@@@@@       @@@@@@@@@@        @@@@@@@@@@   @@@@@@@    @@@@@@
-                @@@@@@@@@@   @@@@@@@%     @@@@@@@@@@@@@     @@@@@@@@@@@@   @@@@@@      @@@@@@@
-              @@@@@@@@@     @@@@@@@      @@@@@@@@@@@@@@    @@@@@@@@@@@@@   @@@@@@       @@@@@@@
-            @@@@@@@@@        @@@@@      @@@@@@@@ @@@@@@   @@@@@@@ @@@@@@@  @@@@@@        @@@@@@@
-          @@@@@@@@@                     @@@@@@   @@@@@@  @@@@@@@  @@@@@@@  @@@@@@        @@@@@@@@
-         @@@@@@@@                      @@@@@@@   @@@@@@  @@@@@@   @@@@@@@  @@@@@@        @@@@@@@@
-       @@@@@@@@       @@@@@           @@@@@@@@  @@@@@@@ @@@@@@@   @@@@@@   @@@@@@         @@@@@@@
-      @@@@@@@@      @@@@@@@@@@@@      @@@@@@@   @@@@@@@ @@@@@@@   @@@@@@   @@@@@@         @@@@@@@
-     @@@@@@@        @@@@@@@@@@@@@@    @@@@@@@  @@@@@@@  @@@@@@@  @@@@@@@   @@@@@@         @@@@@@@
-     @@@@@@         @@@@@@@@@@@@@@@@  @@@@@@%  @@@@@@   @@@@@@@  @@@@@@@   @@@@@@        @@@@@@@
-    @@@@@@@@               @@@@@@@@   @@@@@@  @@@@@@@   @@@@@@@ @@@@@@@    @@@@@@      @@@@@@@@@
-     @@@@@@@           @@@@@@@@@@@@    @@@@@@@@@@@@@@    @@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@@@@@
-      @@@@@@@@@@@@@@@@@@@@@@@@@@@      @@@@@@@@@@@@      @@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@
-       @@@@@@@@@@@@@@@@@@@@@@@          @@@@@@@@@         @@@@@@@@@      @@@@@@@@@@@@@@@@@
-        @@@@@@@@@@@@@@@@@@-               -@@                 @@-          @@@@@@@@@
-            -@@@@@@@           @                  @@@@@@@@@  @@@@@@@   @@@@@@@@   @@@@@@@
-                      @       @@@   @@@@@@@@@@@   @@@@@@@@@ @@@@@@@@  @@@@@@@@@@-@@@@@@@@@@@
-         -@@@@@@@    @@@      @@@  @@@@@@@@@@@@@ @@@@@      @@@@      @@@@@@@@%  @@@@   @@@@@
-       @@@@@@@@@@@%  @@@@@    @@@       @@@@     @@@@@      @@@@      @@@        @@@@    @@@@@
-       @@@@@-        @@@@@@   @@@       @@@@      @@@@@@@@@ @@@@@@@@@ @@@@@@@@@  @@@@   @@@@@@
-       @@@@@@@@@@@   @@@@@@@@@@@@       @@@@      @@@@      @@@@      @@@@@@@@@  @@@@@@@@@@
-            @@@@@@@@ @@@@   @@@@@       @@@@      @@@@     @@@@@      @@@@@      @@@@@@@@@@
-                  @@@ @@@@    @@@@      @@@@      @@@@@    @@@@@      @@@@@      @@@@   @@@@
-     @@@@@@@@@@@@@@@@ @@@@    @@@@ @@@@@@@@@@@@@@@ @@@     @@@@       @@@@@@@@@  @@@@    @@@@
-    @@@@@@@@@@@@@@%   @@@@          @@@@@@@@@@@@                       @@@@@@@@  @@@@     @@@@
-                                                                                 @@@@     @@@@@'''
+ ______  _____   _____  ______       _______ __   _ _____ _______ _______ _______  ______
+|  ____ |     | |     | |     \      |______ | \  |   |   |______ |______ |______ |_____/
+|_____| |_____| |_____| |_____/      ______| |  \_| __|__ |       |       |______ |    \_
+
+
+       .-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.
+       :                                                                       :
+       .     PRESS CTRL+C TO STOP THE SCRIPT AFTER IT STARTS SNIFFING          .
+       .                                                                       .
+       .    run again with 'python goodsniffer.py -o file' to save sniffs      .
+       :                                                                       :
+       `-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=tdwg-'
+                                                                                 '''
 
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("WELCOME TO THE", end="")
-    time.sleep(1)
+    print('WELCOME TO THE', end='')
     print(logo)
-
-    parser = argparse.ArgumentParser(description='A simple packet sniffer')
+    parser = argparse.ArgumentParser(description='The hardest sniffer ever')
     parser.add_argument('-o', '--output', type=str, help='output file')
     args = parser.parse_args()
-
-    capture_eth_data = input("Do you want to capture Raw Ethernet Data (Hexdump) (y for yes, press enter to ignore)? ").lower() == 'y'
+    capture_eth_data = input('Do you want to capture Hexed Ethernet Data (Hexdump) (y for yes, anything else to ignore)? ').lower() == 'y'
+    capture_raweth_data = input('Do you want to capture Raw Ethernet Data (y for yes, anything else to ignore)? ').lower() == 'y'
+    print('STARTING NOW (if this is the only thing visible, then the packets haven\'nt exactly started flowing yet)')
     conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
     conn.bind((interfaceip, 0))
     conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
     serial_number = 0
-    time.sleep(2)
 
-    while True:
-        raw_data, addr = conn.recvfrom(65536)
-        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-        version, header_length, ttl, proto, src, target, data = ipv4_packet(data)
-        timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    t1 = time.time()#timestamp1
 
-        serial_number += 1
+    try:
+        while True:
+            raw_data, addr = conn.recvfrom(65536)
+            dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+            version, header_length, ttl, proto, src, target, data = ipv4_packet(data)
+            timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
-        packet_data = f'===================================\n\nSerial Number: {serial_number}\nTimestamp: {timestamp}\nProtocol number: {(proto)}\nProtocol name: {get_protocol(proto)}\nPacket Length: {len(raw_data)} bytes\nDestination MAC: {dest_mac}\nSource MAC: {src_mac}\nDestination Host: {target}\nSource Host: {src}\nDestination Port: {get_destination_port(data)}\nSource Port: {get_source_port(data)}\n'
+            serial_number += 1
 
-        if capture_eth_data:
-            packet_data += f'Hex Ethernet Data: {data.hex()}\n'
-            packet_data += f'Raw Ethernet Data: {data}\n'
+            packet_data = f'===================================\n\nSerial Number: {serial_number}\nTimestamp: {timestamp}\nProtocol number: {(proto)}\nProtocol name: {get_protocol(proto)}\nPacket Length: {len(raw_data)} bytes\nDestination MAC: {dest_mac}\nSource MAC: {src_mac}\nDestination Host: {target}\nSource Host: {src}\nDestination Port: {get_destination_port(data)}\nSource Port: {get_source_port(data)}\n'
 
-        print(packet_data)
+            if capture_eth_data:
+                packet_data += f'Hex Ethernet Data: {data.hex()}\n'
+            if capture_raweth_data:
+                packet_data += f'Raw Ethernet Data: {data}\n'
 
-        if args.output:
-            write_to_file(args.output + '.txt', packet_data)
-            write_to_csv(args.output + '.csv', [serial_number, timestamp, proto, get_protocol(proto), len(raw_data), dest_mac, src_mac, target, src, get_destination_port(data), get_source_port(data), data.hex() if capture_eth_data else None])
+            print(packet_data)
 
-if __name__ == "__main__":
+            if args.output:
+                write_to_file(args.output + '.txt', packet_data)
+                write_to_csv(args.output + '.csv', [serial_number, timestamp, proto, get_protocol(proto), len(raw_data), dest_mac, src_mac, target, src, get_destination_port(data), get_source_port(data), data.hex() if capture_eth_data else None])
+
+    except KeyboardInterrupt:
+        t2=time.time()#timestamp2
+        t=t2-t1
+        print('\nSniffing stopped successfully\n')
+        print(f'Successfully sniffed  {serial_number} packets in {t} seconds')
+if __name__ == '__main__':
     main()
